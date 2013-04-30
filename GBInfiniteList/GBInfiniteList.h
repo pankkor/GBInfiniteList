00059783dcecb7c3079cb44f0d8db80cdeabb0f5
Luka Mirosevic <luka@goonbee.com>
2013-04-30 11:16:42 +0200

    Initial Commit

---
+++ GBInfiniteList/GBInfiniteList.h
@@ -0,0 +1,13 @@
+//
+//  GBInfiniteList.h
+//  GBInfiniteList
+//
+//  Created by Luka Mirosevic on 30/04/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBInfiniteList : NSObject
+
+@end
---
+++ GBInfiniteList/GBInfiniteList.m
@@ -0,0 +1,13 @@
+//
+//  GBInfiniteList.m
+//  GBInfiniteList
+//
+//  Created by Luka Mirosevic on 30/04/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBInfiniteList.h"
+
+@implementation GBInfiniteList
+
+@end
